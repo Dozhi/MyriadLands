f798613daa7f268e5a428e798eeb9f80b4ca6569
dozhi <zogas@protonmail.com>
2021-09-04 16:32:09 +0000

    fixed typo

--- main.tf
+++ main.tf
@@ -28,5 +28,5 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "myTFVnet"
   address_space       = ["10.0.0.0/16"]
   location            = var.resource_group_location
-  resource_group_name = "azurerm_resource_group"
+  resource_group_name = "res_group"
 }
